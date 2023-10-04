0daf61f9a3f715ff900ca2362753f4035037faed
Steven <stevccru@icloud.com>
2023-10-04 10:54:38 -0400

    first commit

---
+++ Workbook1_Shopping List/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main (String[] args){
+        System.out.println("eggs, cheese, milk, bread, butter, rice, cookies, fruit, soda, chips");
+    }
+}
